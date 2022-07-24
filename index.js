4d9523896ce8b37d6806764850b30c96daf14ac9
mess20 <87274616+mess20@users.noreply.github.com>
2022-07-24 10:39:40 +0800

    Rename index.html to index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-$("body").append("<div id=\"select\" style=\"position:fixed;top:40vh;left:22vw\"><button id=\"option1\" style=\"font-weight:600;border:none;border-radius:30px;background:lightblue;box-shadow:rgba(100,100,255,0.2) 5px 5px 5px 0px;padding:5px 15px;\">手机URL</button><button id=\"option2\" style=\"font-weight:600;margin-left:10px;border:none;border-radius:30px;background:lightblue;box-shadow:rgba(200,200,250,0.3) 5px 5px 5px 0px;padding:5px 15px;\">电脑</button></div>");
+$("body").append("<div id=\"select\" style=\"position:fixed;top:40vh;left:22vw\"><button id=\"option1\" style=\"font-weight:600;border:none;border-radius:30px;background:lightblue;box-shadow:rgba(100,100,255,0.2) 5px 5px 5px 0px;padding:5px 15px;\">手机模式</button><button id=\"option2\" style=\"font-weight:600;margin-left:10px;border:none;border-radius:30px;background:lightblue;box-shadow:rgba(200,200,250,0.3) 5px 5px 5px 0px;padding:5px 15px;\">电脑模式</button></div>");
 
 $("#option1").click( function(){
 $("#select").remove();
